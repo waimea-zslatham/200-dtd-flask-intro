90b1215c92090c64263a81241db340f532294273
waimea-zslatham <zslatham@waimea.school.nz>
2025-05-22 12:24:02 +1200

    Creating Form/404 and Number + Adding to existing

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import redirect
 from random import randint
-
 
 # Create the app
 app = Flask(__name__)
@@ -17,5 +18,19 @@ def about():
 
 @app.get("/random/")
 def random():
-    randNum = randint(1, 1000)
+    randNum = randint(1, 1000000000000000)
     return render_template('pages/random.jinja', number=randNum)
+
+@app.get("/number/<int:num>")
+def analyseNumber(num):
+    print(f"You entered: {num}")
+    return render_template('pages/number.jinja', number=num)
+
+
+@app.get("/form/")
+def form():
+    return render_template('pages/form.jinja')
+
+@app.errorhandler(404)
+def notFound(e):
+    return render_template('pages/404.jinja')
